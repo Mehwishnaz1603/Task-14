711ac5bd1c60356534538ae5184a5018a3844fe2
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 16:30:29 +0500

    Task no 14

---
+++ index.js
@@ -0,0 +1,10 @@
+//////  Task # 14
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would
+// you invite? Make a list that includes at least three people you’d like to 
+//invite to dinner. Then use your list to print a message to each person, 
+//inviting them to dinner.
+let guest_list = ["Bisma", "Ayesha", "Kiran", "Kainat", "Neha"];
+guest_list.forEach(guest_list => {
+    console.log(`Dear ${guest_list}, would you like to join me for dinner?`);
+});
+export {};
